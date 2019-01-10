02c7d23f1bcf34e0a7a15696cfb185ba2eeb0978
yancongwen <ycwen1119@163.com>
2019-01-10 21:21:49 +0800

    add parcel

---
+++ src/button/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="d-button">
+    按钮
+  </button>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='scss' scoped>
+.d-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  cursor: pointer;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
